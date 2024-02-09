4a2aed985fd18c993d973356e800480b2066ffd1
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-02-09 09:04:52 -0500

    Return to Convolutional inputs. Fixup annealing logic

--- pokemonred_puffer/policies.py
+++ pokemonred_puffer/policies.py
@@ -5,14 +5,27 @@ class Recurrent(pufferlib.models.RecurrentWrapper):
     def __init__(self, env, policy, input_size=512, hidden_size=512, num_layers=1):
         super().__init__(env, policy, input_size, hidden_size, num_layers)
 
-
-class Policy(pufferlib.models.ProcgenResnet):
-    def __init__(self, env, cnn_width=16, mlp_width=512):
+class Policy(pufferlib.models.Convolutional):
+    def __init__(self, env, input_size=512, hidden_size=512, output_size=512,
+            framestack=2, flat_size=14336):
         super().__init__(
             env=env,
-            cnn_width=cnn_width,
-            mlp_width=mlp_width,
+            input_size=input_size,
+            hidden_size=hidden_size,
+            output_size=output_size,
+            framestack=framestack,
+            flat_size=flat_size,
+            channels_last=True,
         )
+
+
+# class Policy(pufferlib.models.ProcgenResnet):
+#     def __init__(self, env, cnn_width=16, mlp_width=512):
+#         super().__init__(
+#             env=env,
+#             cnn_width=cnn_width,
+#             mlp_width=mlp_width,
+#         )
 
 '''
 class Policy(pufferlib.models.Policy):
