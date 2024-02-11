a5192e1b20c2708d06935274998a9d9960c6ee2c
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-02-10 22:58:44 -0500

    Move stuff around

--- pokemonred_puffer/environment.py
+++ pokemonred_puffer/environment.py
@@ -538,8 +538,8 @@ class RedGymEnv(Env):
                 "seen_pokemon": int(sum(self.seen_pokemon)),
                 "moves_obtained": int(sum(self.moves_obtained)),
                 "opponent_level": self.max_opponent_level,
+                "reward": self.get_game_state_reward(),
             },
-            "reward": self.get_game_state_reward(),
             "pokemon_exploration_map": self.explore_map
         }
 
