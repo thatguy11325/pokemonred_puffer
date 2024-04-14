62c50c5cd615dde0cc84ef312a886e420d2bb14f
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-04-14 05:39:49 -0400

    handle the scenario until infos gets updated

--- pokemonred_puffer/wrappers/async_io.py
+++ pokemonred_puffer/wrappers/async_io.py
@@ -9,8 +9,9 @@ class AsyncWrapper(gym.Wrapper):
     def __init__(self, env: RedGymEnv, send_queues: list[Queue], recv_queues: list[Queue]):
         super().__init__(env)
         # We need to -1 because the env id is one offset due to puffer's driver env
-        self.send_queue = send_queues[self.env.unwrapped.env_id - 1]
-        self.recv_queue = recv_queues[self.env.unwrapped.env_id - 1]
+        self.send_queue = send_queues[self.env.unwrapped.env_id]
+        self.recv_queue = recv_queues[self.env.unwrapped.env_id]
+        print(f"Initialized queues for {self.env.unwrapped.env_id}")
         # Now we will spawn a thread that will listen for updates
         # and send back when the new state has been loaded
         # this is a slow process and should rarely happen.
@@ -19,6 +20,10 @@ class AsyncWrapper(gym.Wrapper):
         # TODO: Figure out if there's a safe way to exit the thread
 
     def update(self):
-        while new_state := self.recv_queue.get():
-            self.env.update_state(new_state)
-            self.send_queue.put(self.env.unwrapped.env_id - 1)
+        while True:
+            new_state = self.recv_queue.get()
+            if new_state == b"":
+                print(f"invalid state for {self.env.unwrapped.env_id} skipping...")
+            else:
+                self.env.unwrapped.update_state(new_state)
+            self.send_queue.put(self.env.unwrapped.env_id)
