48ac0a83ac4b3612c676208e29f37a17ba8f21fa
drubinstein <pokemonreinforcementlearning@gmail.com>
2024-06-05 21:19:12 -0400

    fix badges again. Add rocket hideout specific event

--- pokemonred_puffer/policies/multi_convolutional.py
+++ pokemonred_puffer/policies/multi_convolutional.py
@@ -69,7 +69,6 @@ class MultiConvolutionalPolicy(pufferlib.models.Policy):
         self.register_buffer(
             "unpack_shift", torch.tensor([6, 4, 2, 0], dtype=torch.uint8), persistent=False
         )
-        self.register_buffer("binary_mask", torch.tensor([2**i for i in range(8)]))
 
     def encode_observations(self, observations):
         observations = unpack_batched_obs(observations, self.unflatten_context)
@@ -99,8 +98,7 @@ class MultiConvolutionalPolicy(pufferlib.models.Policy):
                 .flatten()
                 .int(),
             ).reshape(restored_shape)
-        # > 0 doesn't risk a type conversion
-        badges = (observations["badges"] & self.binary_mask) > 0
+        badges = (torch.arange(8) + 1) <= observations["badges"]
 
         image_observation = torch.cat((screen, visited_mask, global_map), dim=-1)
         if self.channels_last:
