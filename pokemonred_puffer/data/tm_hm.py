5735754504eb2460198fd7ebce6b2245e723d9ee
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-06-10 01:24:09 -0400

    Add forgotten data for important rom values

---
+++ pokemonred_puffer/data/tm_hm.py
@@ -0,0 +1,194 @@
+from pokemonred_puffer.data.species import SPECIES_IDS
+
+
+TM_HM_MOVES = {
+    5,  # Mega punch
+    0xD,  # Razor wind
+    0xE,  # Swords dance
+    0x12,  # Whirlwind
+    0x19,  # Mega kick
+    0x5C,  # Toxic
+    0x20,  # Horn drill
+    0x22,  # Body slam
+    0x24,  # Take down
+    0x26,  # Double edge
+    0x3D,  # Bubble beam
+    0x37,  # Water gun
+    0x3A,  # Ice beam
+    0x3B,  # Blizzard
+    0x3F,  # Hyper beam
+    0x06,  # Pay day
+    0x42,  # Submission
+    0x44,  # Counter
+    0x45,  # Seismic toss
+    0x63,  # Rage
+    0x48,  # Mega drain
+    0x4C,  # Solar beam
+    0x52,  # Dragon rage
+    0x55,  # Thunderbolt
+    0x57,  # Thunder
+    0x59,  # Earthquake
+    0x5A,  # Fissure
+    0x5B,  # Dig
+    0x5E,  # Psychic
+    0x64,  # Teleport
+    0x66,  # Mimic
+    0x68,  # Double team
+    0x73,  # Reflect
+    0x75,  # Bide
+    0x76,  # Metronome
+    0x78,  # Selfdestruct
+    0x79,  # Egg bomb
+    0x7E,  # Fire blast
+    0x81,  # Swift
+    0x82,  # Skull bash
+    0x87,  # Softboiled
+    0x8A,  # Dream eater
+    0x8F,  # Sky attack
+    0x9C,  # Rest
+    0x56,  # Thunder wave
+    0x95,  # Psywave
+    0x99,  # Explosion
+    0x9D,  # Rock slide
+    0xA1,  # Tri attack
+    0xA4,  # Substitute
+    0x0F,  # Cut
+    0x13,  # Fly
+    0x39,  # Surf
+    0x46,  # Strength
+    0x94,  # Flash
+}
+
+
+CUT_SPECIES_IDS = {
+    SPECIES_IDS["BULBASAUR"],
+    SPECIES_IDS["IVYSAUR"],
+    SPECIES_IDS["VENUSAUR"],
+    SPECIES_IDS["CHARMANDER"],
+    SPECIES_IDS["CHARMELEON"],
+    SPECIES_IDS["CHARIZARD"],
+    SPECIES_IDS["BEEDRILL"],
+    SPECIES_IDS["SANDSHREW"],
+    SPECIES_IDS["SANDSLASH"],
+    SPECIES_IDS["ODDISH"],
+    SPECIES_IDS["GLOOM"],
+    SPECIES_IDS["VILEPLUME"],
+    SPECIES_IDS["PARAS"],
+    SPECIES_IDS["PARASECT"],
+    SPECIES_IDS["BELLSPROUT"],
+    SPECIES_IDS["WEEPINBELL"],
+    SPECIES_IDS["VICTREEBEL"],
+    SPECIES_IDS["TENTACOOL"],
+    SPECIES_IDS["TENTACRUEL"],
+    SPECIES_IDS["FARFETCHD"],
+    SPECIES_IDS["KRABBY"],
+    SPECIES_IDS["KINGLER"],
+    SPECIES_IDS["LICKITUNG"],
+    SPECIES_IDS["TANGELA"],
+    SPECIES_IDS["SCYTHER"],
+    SPECIES_IDS["PINSIR"],
+    SPECIES_IDS["MEW"],
+}
+
+SURF_SPECIES_IDS = {
+    SPECIES_IDS["SQUIRTLE"],
+    SPECIES_IDS["WARTORTLE"],
+    SPECIES_IDS["BLASTOISE"],
+    SPECIES_IDS["NIDOQUEEN"],
+    SPECIES_IDS["NIDOKING"],
+    SPECIES_IDS["PSYDUCK"],
+    SPECIES_IDS["GOLDUCK"],
+    SPECIES_IDS["POLIWAG"],
+    SPECIES_IDS["POLIWHIRL"],
+    SPECIES_IDS["POLIWRATH"],
+    SPECIES_IDS["TENTACOOL"],
+    SPECIES_IDS["TENTACRUEL"],
+    SPECIES_IDS["SLOWPOKE"],
+    SPECIES_IDS["SLOWBRO"],
+    SPECIES_IDS["SEEL"],
+    SPECIES_IDS["DEWGONG"],
+    SPECIES_IDS["SHELLDER"],
+    SPECIES_IDS["CLOYSTER"],
+    SPECIES_IDS["KRABBY"],
+    SPECIES_IDS["KINGLER"],
+    SPECIES_IDS["LICKITUNG"],
+    SPECIES_IDS["RHYDON"],
+    SPECIES_IDS["KANGASKHAN"],
+    SPECIES_IDS["HORSEA"],
+    SPECIES_IDS["SEADRA"],
+    SPECIES_IDS["GOLDEEN"],
+    SPECIES_IDS["SEAKING"],
+    SPECIES_IDS["STARYU"],
+    SPECIES_IDS["STARMIE"],
+    SPECIES_IDS["GYARADOS"],
+    SPECIES_IDS["LAPRAS"],
+    SPECIES_IDS["VAPOREON"],
+    SPECIES_IDS["OMANYTE"],
+    SPECIES_IDS["OMASTAR"],
+    SPECIES_IDS["KABUTO"],
+    SPECIES_IDS["KABUTOPS"],
+    SPECIES_IDS["SNORLAX"],
+    SPECIES_IDS["DRATINI"],
+    SPECIES_IDS["DRAGONAIR"],
+    SPECIES_IDS["DRAGONITE"],
+    SPECIES_IDS["MEW"],
+}
+
+STRENGTH_SPECIES_IDS = {
+    SPECIES_IDS["CHARMANDER"],
+    SPECIES_IDS["CHARMELEON"],
+    SPECIES_IDS["CHARIZARD"],
+    SPECIES_IDS["SQUIRTLE"],
+    SPECIES_IDS["WARTORTLE"],
+    SPECIES_IDS["BLASTOISE"],
+    SPECIES_IDS["EKANS"],
+    SPECIES_IDS["ARBOK"],
+    SPECIES_IDS["SANDSHREW"],
+    SPECIES_IDS["SANDSLASH"],
+    SPECIES_IDS["NIDOQUEEN"],
+    SPECIES_IDS["NIDOKING"],
+    SPECIES_IDS["CLEFAIRY"],
+    SPECIES_IDS["CLEFABLE"],
+    SPECIES_IDS["JIGGLYPUFF"],
+    SPECIES_IDS["WIGGLYTUFF"],
+    SPECIES_IDS["PSYDUCK"],
+    SPECIES_IDS["GOLDUCK"],
+    SPECIES_IDS["MANKEY"],
+    SPECIES_IDS["PRIMEAPE"],
+    SPECIES_IDS["POLIWHIRL"],
+    SPECIES_IDS["POLIWRATH"],
+    SPECIES_IDS["MACHOP"],
+    SPECIES_IDS["MACHOKE"],
+    SPECIES_IDS["MACHAMP"],
+    SPECIES_IDS["GEODUDE"],
+    SPECIES_IDS["GRAVELER"],
+    SPECIES_IDS["GOLEM"],
+    SPECIES_IDS["SLOWPOKE"],
+    SPECIES_IDS["SLOWBRO"],
+    SPECIES_IDS["SEEL"],
+    SPECIES_IDS["DEWGONG"],
+    SPECIES_IDS["GENGAR"],
+    SPECIES_IDS["ONIX"],
+    SPECIES_IDS["KRABBY"],
+    SPECIES_IDS["KINGLER"],
+    SPECIES_IDS["EXEGGUTOR"],
+    SPECIES_IDS["CUBONE"],
+    SPECIES_IDS["MAROWAK"],
+    SPECIES_IDS["HITMONLEE"],
+    SPECIES_IDS["HITMONCHAN"],
+    SPECIES_IDS["LICKITUNG"],
+    SPECIES_IDS["RHYHORN"],
+    SPECIES_IDS["RHYDON"],
+    SPECIES_IDS["CHANSEY"],
+    SPECIES_IDS["KANGASKHAN"],
+    SPECIES_IDS["ELECTABUZZ"],
+    SPECIES_IDS["MAGMAR"],
+    SPECIES_IDS["PINSIR"],
+    SPECIES_IDS["TAUROS"],
+    SPECIES_IDS["GYARADOS"],
+    SPECIES_IDS["LAPRAS"],
+    SPECIES_IDS["SNORLAX"],
+    SPECIES_IDS["DRAGONITE"],
+    SPECIES_IDS["MEWTWO"],
+    SPECIES_IDS["MEW"],
+}
