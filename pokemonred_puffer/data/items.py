0d65fac4550d28c4e29b04d7412faadfdb2b87bd
drubinstein <pokemonreinforcementlearning@gmail.com>
2024-06-10 01:24:09 -0400

    Add forgotten data for important rom values

---
+++ pokemonred_puffer/data/items.py
@@ -0,0 +1,256 @@
+MAX_ITEM_CAPACITY = 20
+
+# Starts at 0x1
+KEY_ITEM_IDS = [
+    False,  # MASTER_BALL
+    False,  # ULTRA_BALL
+    False,  # GREAT_BALL
+    False,  # POKE_BALL
+    True,  # TOWN_MAP
+    True,  # BICYCLE
+    True,  # SURFBOARD
+    True,  # SAFARI_BALL
+    True,  # POKEDEX
+    False,  # MOON_STONE
+    False,  # ANTIDOTE
+    False,  # BURN_HEAL
+    False,  # ICE_HEAL
+    False,  # AWAKENING
+    False,  # PARLYZ_HEAL
+    False,  # FULL_RESTORE
+    False,  # MAX_POTION
+    False,  # HYPER_POTION
+    False,  # SUPER_POTION
+    False,  # POTION
+    True,  # BOULDERBADGE
+    True,  # CASCADEBADGE
+    True,  # THUNDERBADGE
+    True,  # RAINBOWBADGE
+    True,  # SOULBADGE
+    True,  # MARSHBADGE
+    True,  # VOLCANOBADGE
+    True,  # EARTHBADGE
+    False,  # ESCAPE_ROPE
+    False,  # REPEL
+    True,  # OLD_AMBER
+    False,  # FIRE_STONE
+    False,  # THUNDER_STONE
+    False,  # WATER_STONE
+    False,  # HP_UP
+    False,  # PROTEIN
+    False,  # IRON
+    False,  # CARBOS
+    False,  # CALCIUM
+    False,  # RARE_CANDY
+    True,  # DOME_FOSSIL
+    True,  # HELIX_FOSSIL
+    True,  # SECRET_KEY
+    True,  # ITEM_2C
+    True,  # BIKE_VOUCHER
+    False,  # X_ACCURACY
+    False,  # LEAF_STONE
+    True,  # CARD_KEY
+    False,  # NUGGET
+    False,  # ITEM_32
+    False,  # POKE_DOLL
+    False,  # FULL_HEAL
+    False,  # REVIVE
+    False,  # MAX_REVIVE
+    False,  # GUARD_SPEC
+    False,  # SUPER_REPEL
+    False,  # MAX_REPEL
+    False,  # DIRE_HIT
+    False,  # COIN
+    False,  # FRESH_WATER
+    False,  # SODA_POP
+    False,  # LEMONADE
+    True,  # S_S_TICKET
+    True,  # GOLD_TEETH
+    False,  # X_ATTACK
+    False,  # X_DEFEND
+    False,  # X_SPEED
+    False,  # X_SPECIAL
+    True,  # COIN_CASE
+    True,  # OAKS_PARCEL
+    True,  # ITEMFINDER
+    True,  # SILPH_SCOPE
+    True,  # POKE_FLUTE
+    True,  # LIFT_KEY
+    False,  # EXP_ALL
+    True,  # OLD_ROD
+    True,  # GOOD_ROD
+    True,  # SUPER_ROD
+    False,  # PP_UP
+    False,  # ETHER
+    False,  # MAX_ETHER
+    False,  # ELIXER
+    False,  # MAX_ELIXER
+]
+
+# Start = 0x1
+ITEM_NAMES = [
+    "MASTER_BALL",
+    "ULTRA_BALL",
+    "GREAT_BALL",
+    "POKE_BALL",
+    "TOWN_MAP",
+    "BICYCLE",
+    "SURFBOARD",
+    "SAFARI_BALL",
+    "POKEDEX",
+    "MOON_STONE",
+    "ANTIDOTE",
+    "BURN_HEAL",
+    "ICE_HEAL",
+    "AWAKENING",
+    "PARLYZ_HEAL",
+    "FULL_RESTORE",
+    "MAX_POTION",
+    "HYPER_POTION",
+    "SUPER_POTION",
+    "POTION",
+    "BOULDERBADGE",
+    "CASCADEBADGE",
+    "THUNDERBADGE",
+    "RAINBOWBADGE",
+    "SOULBADGE",
+    "MARSHBADGE",
+    "VOLCANOBADGE",
+    "EARTHBADGE",
+    "ESCAPE_ROPE",
+    "REPEL",
+    "OLD_AMBER",
+    "FIRE_STONE",
+    "THUNDER_STONE",
+    "WATER_STONE",
+    "HP_UP",
+    "PROTEIN",
+    "IRON",
+    "CARBOS",
+    "CALCIUM",
+    "RARE_CANDY",
+    "DOME_FOSSIL",
+    "HELIX_FOSSIL",
+    "SECRET_KEY",
+    "UNUSED_ITEM",
+    "BIKE_VOUCHER",
+    "X_ACCURACY",
+    "LEAF_STONE",
+    "CARD_KEY",
+    "NUGGET",
+    "PP_UP_2",
+    "POKE_DOLL",
+    "FULL_HEAL",
+    "REVIVE",
+    "MAX_REVIVE",
+    "GUARD_SPEC",
+    "SUPER_REPEL",
+    "MAX_REPEL",
+    "DIRE_HIT",
+    "COIN",
+    "FRESH_WATER",
+    "SODA_POP",
+    "LEMONADE",
+    "S_S_TICKET",
+    "GOLD_TEETH",
+    "X_ATTACK",
+    "X_DEFEND",
+    "X_SPEED",
+    "X_SPECIAL",
+    "COIN_CASE",
+    "OAKS_PARCEL",
+    "ITEMFINDER",
+    "SILPH_SCOPE",
+    "POKE_FLUTE",
+    "LIFT_KEY",
+    "EXP_ALL",
+    "OLD_ROD",
+    "GOOD_ROD",
+    "SUPER_ROD",
+    "PP_UP",
+    "ETHER",
+    "MAX_ETHER",
+    "ELIXER",
+    "MAX_ELIXER",
+    "FLOOR_B2F",
+    "FLOOR_B1F",
+    "FLOOR_1F",
+    "FLOOR_2F",
+    "FLOOR_3F",
+    "FLOOR_4F",
+    "FLOOR_5F",
+    "FLOOR_6F",
+    "FLOOR_7F",
+    "FLOOR_8F",
+    "FLOOR_9F",
+    "FLOOR_10F",
+    "FLOOR_11F",
+    "FLOOR_B4F",
+]
+
+# Start = 0xC4
+TM_HM_ITEM_IDS = [
+    "HM_01",
+    "HM_02",
+    "HM_03",
+    "HM_04",
+    "HM_05",
+    "TM_01",
+    "TM_02",
+    "TM_03",
+    "TM_04",
+    "TM_05",
+    "TM_06",
+    "TM_07",
+    "TM_08",
+    "TM_09",
+    "TM_10",
+    "TM_11",
+    "TM_12",
+    "TM_13",
+    "TM_14",
+    "TM_15",
+    "TM_16",
+    "TM_17",
+    "TM_18",
+    "TM_19",
+    "TM_20",
+    "TM_21",
+    "TM_22",
+    "TM_23",
+    "TM_24",
+    "TM_25",
+    "TM_26",
+    "TM_27",
+    "TM_28",
+    "TM_29",
+    "TM_30",
+    "TM_31",
+    "TM_32",
+    "TM_33",
+    "TM_34",
+    "TM_35",
+    "TM_36",
+    "TM_37",
+    "TM_38",
+    "TM_39",
+    "TM_40",
+    "TM_41",
+    "TM_42",
+    "TM_43",
+    "TM_44",
+    "TM_45",
+    "TM_46",
+    "TM_47",
+    "TM_48",
+    "TM_49",
+    "TM_50",
+]
+
+HM_ITEM_IDS = {0xC4, 0xC5, 0xC6, 0xC7, 0xC8}
+
+ITEM_NAME_TO_ID = (
+    {v: i + 0x1 for i, v in enumerate(ITEM_NAMES)}
+    | {v: i + 0xC4 for i, v in enumerate(TM_HM_ITEM_IDS)}
+    | {"SAFARI_BAIT": 0x15, "SAFARI_ROCK": 0x16}
+)
