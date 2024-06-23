3f2ab67faeaf211fa079d71545639066e815342a
drubinstein <pokemonreinforcementlearning@gmail.com>
2024-06-22 23:09:54 -0400

    remove attempt at cleanly stopping stream wrapper

--- pokemonred_puffer/wrappers/stream_wrapper.py
+++ pokemonred_puffer/wrappers/stream_wrapper.py
@@ -30,15 +30,6 @@ class StreamWrapper(gym.Wrapper):
         else:
             raise Exception("Could not find emulator!")
 
-    def __del__(self):
-        try:
-            self.loop.run_until_complete()
-            for task in asyncio.all_tasks():
-                task.cancel()
-            self.loop.close()
-        except RuntimeError:
-            pass
-
     def step(self, action):
         x_pos = self.env.unwrapped.read_m("wXCoord")
         y_pos = self.env.unwrapped.read_m("wYCoord")
