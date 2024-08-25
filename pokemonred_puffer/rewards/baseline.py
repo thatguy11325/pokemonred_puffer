64e98eca762de25b98dc344a08501a35e2691dbf
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-08-24 22:14:08 -0600

    map id rewards

--- pokemonred_puffer/rewards/baseline.py
+++ pokemonred_puffer/rewards/baseline.py
@@ -361,3 +361,65 @@ class ObjectRewardRequiredEventsEnvTilesetExploration(BaselineRewardEnv):
             return self.max_level_sum
         else:
             return 15 + (self.max_level_sum - 15) / 4
+
+
+class ObjectRewardRequiredEventsMapIds(BaselineRewardEnv):
+    def get_game_state_reward(self):
+        _, wBagItems = self.pyboy.symbol_lookup("wBagItems")
+        numBagItems = self.read_m("wNumBagItems")
+        bag_item_ids = set(self.pyboy.memory[wBagItems : wBagItems + 2 * numBagItems : 2])
+
+        return (
+            {
+                "event": self.reward_config["event"] * self.update_max_event_rew(),
+                "seen_pokemon": self.reward_config["seen_pokemon"] * sum(self.seen_pokemon),
+                "caught_pokemon": self.reward_config["caught_pokemon"] * sum(self.caught_pokemon),
+                "moves_obtained": self.reward_config["moves_obtained"] * sum(self.moves_obtained),
+                "hm_count": self.reward_config["hm_count"] * self.get_hm_count(),
+                "level": self.reward_config["level"] * self.get_levels_reward(),
+                "badges": self.reward_config["badges"] * self.get_badges(),
+                "cut_coords": self.reward_config["cut_coords"] * sum(self.cut_coords.values()),
+                "cut_tiles": self.reward_config["cut_tiles"] * sum(self.cut_tiles.values()),
+                "start_menu": self.reward_config["start_menu"] * self.seen_start_menu,
+                "pokemon_menu": self.reward_config["pokemon_menu"] * self.seen_pokemon_menu,
+                "stats_menu": self.reward_config["stats_menu"] * self.seen_stats_menu,
+                "bag_menu": self.reward_config["bag_menu"] * self.seen_bag_menu,
+                "explore_hidden_objs": sum(self.seen_hidden_objs.values()),
+                "explore_signs": sum(self.seen_signs.values())
+                * self.reward_config["explore_signs"],
+                "seen_action_bag_menu": self.seen_action_bag_menu
+                * self.reward_config["seen_action_bag_menu"],
+                "pokecenter_heal": self.pokecenter_heal * self.reward_config["pokecenter_heal"],
+                "rival3": self.reward_config["required_event"]
+                * int(self.read_m("wSSAnne2FCurScript") == 4),
+                "game_corner_rocket": self.reward_config["required_event"]
+                * float(self.missables.get_missable("HS_GAME_CORNER_ROCKET")),
+                "saffron_guard": self.reward_config["required_event"]
+                * float(self.wd728.get_bit("GAVE_SAFFRON_GUARD_DRINK")),
+                "a_press": len(self.a_press) * self.reward_config["a_press"],
+                "warps": len(self.seen_warps) * self.reward_config["explore_warps"],
+                "use_surf": self.reward_config["use_surf"] * self.use_surf,
+                "exploration": self.reward_config["exploration"] * np.sum(self.reward_explore_map),
+            }
+            | {
+                event: self.reward_config["required_event"] * float(self.events.get_event(event))
+                for event in REQUIRED_EVENTS
+            }
+            | {
+                item.name: self.reward_config["required_item"] * float(item.value in bag_item_ids)
+                for item in REQUIRED_ITEMS
+            }
+            | {
+                item.name: self.reward_config["useful_item"] * float(item.value in bag_item_ids)
+                for item in USEFUL_ITEMS
+            }
+        )
+
+    def get_levels_reward(self):
+        party_size = self.read_m("wPartyCount")
+        party_levels = [self.read_m(f"wPartyMon{i+1}Level") for i in range(party_size)]
+        self.max_level_sum = max(self.max_level_sum, sum(party_levels))
+        if self.max_level_sum < 15:
+            return self.max_level_sum
+        else:
+            return 15 + (self.max_level_sum - 15) / 4
