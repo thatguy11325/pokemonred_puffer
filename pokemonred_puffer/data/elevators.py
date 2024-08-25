70619bf57fcf54d734ed3f08d1bf75731a80ad06
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-08-24 22:18:04 -0600

    Add missing elevators data

---
+++ pokemonred_puffer/data/elevators.py
@@ -0,0 +1,26 @@
+from pokemonred_puffer.data.map import MapIds
+
+
+# A mapping of map id to the next id in the item list
+NEXT_ELEVATORS = {
+    MapIds.ROCKET_HIDEOUT_B1F: 1,
+    MapIds.ROCKET_HIDEOUT_B2F: 2,
+    MapIds.ROCKET_HIDEOUT_B3F: 3,
+    MapIds.ROCKET_HIDEOUT_B4F: 0,
+    MapIds.CELADON_MART_1F: 1,
+    MapIds.CELADON_MART_2F: 2,
+    MapIds.CELADON_MART_3F: 3,
+    MapIds.CELADON_MART_4F: 4,
+    MapIds.CELADON_MART_5F: 0,
+    MapIds.SILPH_CO_1F: 1,
+    MapIds.SILPH_CO_2F: 2,
+    MapIds.SILPH_CO_3F: 3,
+    MapIds.SILPH_CO_4F: 4,
+    MapIds.SILPH_CO_5F: 5,
+    MapIds.SILPH_CO_6F: 6,
+    MapIds.SILPH_CO_7F: 7,
+    MapIds.SILPH_CO_8F: 8,
+    MapIds.SILPH_CO_9F: 9,
+    MapIds.SILPH_CO_10F: 10,
+    MapIds.SILPH_CO_11F: 0,
+}
