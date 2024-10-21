e5c6007df1e0c22a909a17d7010689d64da3a631
drubinstein <pokemonreinforcementlearning@gmail.com>
2024-10-20 22:57:25 -0400

    add pid to schema

--- pokemonred_puffer/train.py
+++ pokemonred_puffer/train.py
@@ -362,7 +362,7 @@ def train(
                 with sqlite3.connect(db_filename) as conn:
                     cur = conn.cursor()
                     cur.execute(
-                        "CREATE TABLE states(env_id INT PRIMARY_KEY, pyboy_state BLOB, reset BOOLEAN);"
+                        "CREATE TABLE states(env_id INT PRIMARY_KEY, pyboy_state BLOB, reset BOOLEAN, pid INT);"
                     )
 
             vecenv = pufferlib.vector.make(
