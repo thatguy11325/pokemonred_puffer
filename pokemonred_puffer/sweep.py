208d2996fde94ce83209efac0a768e5931dfc1c9
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2024-10-28 08:16:29 -0400

    Playing around with more settings

--- pokemonred_puffer/sweep.py
+++ pokemonred_puffer/sweep.py
@@ -99,8 +99,9 @@ def launch_sweep(
             better_direction_sign=1,
             is_wandb_logging_enabled=False,
             wandb_params=WandbLoggingParams(project_name="Pokemon", run_name=sweep_id),
-            resample_frequency=25,
+            resample_frequency=5,
             num_random_samples=len(params),
+            is_pareto_group_selection_conservative=False,
         )
 
         carbs = CARBS(config=config, params=params)
