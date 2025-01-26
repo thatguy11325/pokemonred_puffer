c45169b50db825c0e8600af4c3f9aca78ad21278
thatguy11325 <148832074+thatguy11325@users.noreply.github.com>
2025-01-26 11:03:34 -0500

    experiment: remove some required events

--- pokemonred_puffer/data/events.py
+++ pokemonred_puffer/data/events.py
@@ -2650,10 +2650,10 @@ REQUIRED_EVENTS = {
     "EVENT_BEAT_BLAINE",
     "EVENT_BEAT_SABRINA",
     # "EVENT_GOT_HM05",
-    "EVENT_FIGHT_ROUTE12_SNORLAX",
-    "EVENT_BEAT_ROUTE12_SNORLAX",
-    "EVENT_FIGHT_ROUTE16_SNORLAX",
-    "EVENT_BEAT_ROUTE16_SNORLAX",
+    # "EVENT_FIGHT_ROUTE12_SNORLAX",
+    # "EVENT_BEAT_ROUTE12_SNORLAX",
+    # "EVENT_FIGHT_ROUTE16_SNORLAX",
+    # "EVENT_BEAT_ROUTE16_SNORLAX",
     # "EVENT_GOT_HM02",
     "EVENT_RESCUED_MR_FUJI",
     "EVENT_2ND_ROUTE22_RIVAL_BATTLE",
@@ -2688,15 +2688,15 @@ REQUIRED_EVENTS = {
     "ELITE4_CHAMPION_EVENTS_END",
     # Random trainers we need to beat
     # lass at the entrance of route 9
-    "EVENT_BEAT_ROUTE_9_TRAINER_0",
-    # exploding graveler trainer in rock tunnel
-    "EVENT_BEAT_ROCK_TUNNEL_2_TRAINER_1",
-    # lass at the end of rock tunnel
-    "EVENT_BEAT_ROCK_TUNNEL_1_TRAINER_5",
-    # Rock tunnel super nerd
-    "EVENT_BEAT_ROCK_TUNNEL_1_TRAINER_3",
-    # second rock tunnel super nerd
-    "EVENT_BEAT_ROCK_TUNNEL_2_TRAINER_7",
-    # required rock tunnel trainer
-    "EVENT_BEAT_ROCK_TUNNEL_2_TRAINER_5",
+    # "EVENT_BEAT_ROUTE_9_TRAINER_0",
+    # # exploding graveler trainer in rock tunnel
+    # "EVENT_BEAT_ROCK_TUNNEL_2_TRAINER_1",
+    # # lass at the end of rock tunnel
+    # "EVENT_BEAT_ROCK_TUNNEL_1_TRAINER_5",
+    # # Rock tunnel super nerd
+    # "EVENT_BEAT_ROCK_TUNNEL_1_TRAINER_3",
+    # # second rock tunnel super nerd
+    # "EVENT_BEAT_ROCK_TUNNEL_2_TRAINER_7",
+    # # required rock tunnel trainer
+    # "EVENT_BEAT_ROCK_TUNNEL_2_TRAINER_5",
 }
