2c5377eb2ac7b810a3ab580018618cb6541c8714
drubinstein <pokemonreinforcementlearning@gmail.com>
2025-02-28 07:31:47 -0500

    replay

--- pokemonred_puffer/wrappers/coords_writer.py
+++ pokemonred_puffer/wrappers/coords_writer.py
@@ -1,6 +1,5 @@
 import collections
 import os
-from datetime import datetime
 from typing import cast
 
 import gymnasium as gym
@@ -14,28 +13,44 @@ class CoordinatesWriter(gym.Wrapper):
         super().__init__(env)
         self.coord_list = collections.deque()
         self.output_dir: str = config.output_dir
+        self.record_interval = 500
+        self.step_counter = 0
         self.write_frequency: int = config.write_frequency
-        self.folder_name = datetime.today().strftime("%Y%m%d-%H%M%S")
-        self.save_dir = os.path.join(self.output_dir, self.folder_name)
-        os.makedirs(self.save_dir, exist_ok=True)
+        self.write_path = (
+            os.path.join(self.output_dir, str(cast(RedGymEnv, self.env).env_id) + "-coords.csv"),
+        )
+        os.makedirs(self.output_dir, exist_ok=True)
+        with open(self.write_path, "w") as f:
+            f.write("")
 
     def step(self, action):
         map_n = self.env.unwrapped.read_m("wCurMap")
         y_pos = self.env.unwrapped.read_m("wYCoord")
         x_pos = self.env.unwrapped.read_m("wXCoord")
-        self.coord_list.append((map_n, y_pos, x_pos))
+        if self.step_counter >= self.upload_interval:
+            self.coord_list.append([str(map_n), str(y_pos), str(x_pos)])
+            self.step_counter = 0
         if len(self.coord_list) >= self.write_frequency:
-            with open(
-                os.path.join(self.save_dir, str(cast(RedGymEnv, self.env).env_id) + "-coords.csv"),
-                "a",
-            ) as f:
-                f.writelines(",".join(str(coord)) + "\n" for coord in self.coord_list)
-                self.coord_list.clear()
+            self.write()
+            self.coord_list.clear()
+
+        self.step_counter += 1
 
         return self.env.step(action)
 
     def reset(self, *args, **kwargs):
         return self.env.reset(*args, **kwargs)
+
+    def close(self):
+        self.write()
+        return self.env.close()
+
+    def write(self):
+        with open(
+            self.write_path,
+            "a",
+        ) as f:
+            f.writelines(",".join(coord) + "\n" for coord in self.coord_list)
 
 
 class ActionsWriter(gym.Wrapper):
@@ -44,25 +59,31 @@ class ActionsWriter(gym.Wrapper):
         self.action_list = collections.deque()
         self.output_dir: str = config.output_dir
         self.write_frequency: int = config.write_frequency
-        self.folder_name = datetime.today().strftime("%Y%m%d-%H%M%S")
-        self.save_dir = os.path.join(self.output_dir, self.folder_name)
-        os.makedirs(self.save_dir, exist_ok=True)
+        self.write_path = os.path.join(
+            self.output_dir, str(cast(RedGymEnv, self.env).env_id) + "-actions.csv"
+        )
+        os.makedirs(self.output_dir, exist_ok=True)
+        with open(self.write_path, "w") as f:
+            f.write("")
 
     def step(self, action):
-        map_n = self.env.unwrapped.read_m("wCurMap")
-        y_pos = self.env.unwrapped.read_m("wYCoord")
-        x_pos = self.env.unwrapped.read_m("wXCoord")
-        self.action_list.append((map_n, y_pos, x_pos))
-        if len(self.coord_list) >= self.write_frequency:
-            os.makedirs()
-            with open(
-                os.path.join(self.save_dir, str(cast(RedGymEnv, self.env).env_id) + "-actions.csv"),
-                "a",
-            ) as f:
-                f.writelines(str(str(action)) + "\n" for action in self.action_list)
-                self.action_list.clear()
+        self.action_list.append(action)
+        if len(self.action_list) >= self.write_frequency:
+            self.write()
+            self.action_list.clear()
 
         return self.env.step(action)
 
     def reset(self, *args, **kwargs):
         return self.env.reset(*args, **kwargs)
+
+    def close(self):
+        self.write()
+        return self.env.close()
+
+    def write(self):
+        with open(
+            self.write_path,
+            "a",
+        ) as f:
+            f.writelines(str(action) + "\n" for action in self.action_list)
